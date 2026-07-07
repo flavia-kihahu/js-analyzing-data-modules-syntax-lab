2e0013d8506bacca001d0fa8dbc5c9743a908825
flavia-kihahu <kihahuflavia@gmail.com>
2026-07-07 20:01:15 +0300

    syntax-lab

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-
+function combineUsers(...args) {
+  let combinedObject = {
+    users: [],
+  };
+  for (let i = 0; i < args.length; i++) {
+    combinedObject.users = [...combinedObject.users, ...args[i]];
+  }
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+  return combinedObject;
+}
 
 
 module.exports = {
